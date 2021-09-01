9df117532d24d08c46c7bdb3f0a32c707c621268
libralog <87793589+libralog@users.noreply.github.com>
2021-09-01 12:55:12 +0530

    Create recon.sh

---
+++ recon.sh
@@ -0,0 +1,568 @@
+#!/bin/bash
+
+range=$1
+
+resolvers="/root/tools/dnsvalidator/resolvers.txt"
+
+worlist="/root/tools/SecLists/Discovery/DNS/bitquark-subdomains-top100000.txt"
+
+resolve_domain="massdns -r /root/tools/massdns/lists/resolvers.txt -t A -o S -w"
+
+
+domain_enum(){
+
+
+	for domain in $(cat $range);
+
+	do
+
+		mkdir -p $domain $domain/sources $domain/Recon/hidden_dir $domain/Recon/nuclei $domain/Recon/wayback $domain/Recon/gf $domain/Recon/wordlist $domain/Recon/masscan $domain/Recon/vulnerabilities $domain/Recon/scree_shoort $domain/Recon/API_secret $domain/Recon/git-dorking
+    
+        cd /root/basic-recon/$domain/Recon/git-dorking/ && touch dorking.txt && cd  /root/basic-recon/
+
+        cd /root/basic-recon/$domain/Recon/vulnerabilities && touch dalfox_xss.txt && cd  /root/basic-recon/
+
+        cd /root/basic-recon/$domain/Recon/sources && touch bucket.txt && cd  /root/basic-recon/
+
+		printf "\n\n---------------------------------------------\n"
+		printf "          \e[33mSubfinder Is Starting ...\e[0m                  "
+		printf "\n---------------------------------------------\n\n"
+
+		subfinder -d $domain -t 100 -v -o $domain/sources/subfinder.txt
+
+		printf "\n\n---------------------------------------------\n"
+		printf "          \e[33mAsset Finder Is Starting ...\e[0m                  "
+		printf "\n---------------------------------------------\n\n"
+
+		assetfinder -subs-only $domain | tee $domain/sources/assetfinder.txt
+
+		printf "\n\n---------------------------------------------\n"
+		printf "          \e[33mAmass Enum Is Starting ...\e[0m                  "
+		printf "\n---------------------------------------------\n\n"
+
+		amass enum -passive -d $domain -o $domain/sources/amass.txt
+
+		printf "\n\n---------------------------------------------\n"
+		printf "          \e[33mCrt.sh Is Starting ...\e[0m                  "
+		printf "\n---------------------------------------------\n\n"
+
+		./crt.sh $domain && mv $domain.txt $domain/sources/crt.txt
+
+		printf "\n\n---------------------------------------------\n"
+		printf "          \e[33mShuffledns Is Starting...\e[0m                  "
+		printf "\n---------------------------------------------\n\n"
+
+		shuffledns -d $domain -w $worlist -r $resolvers -o $domain/sources/shuffledns.txt -wt 9000 -t 1000 
+
+		printf "\n\n---------------------------------------------\n"
+		printf "          \e[33mSorting Is Starting ...\e[0m                  "
+		printf "\n---------------------------------------------\n\n"
+
+		cd /root/basic-recon/$domain/sources/ && sort subfinder.txt assetfinder.txt amass.txt  crt.txt shuffledns.txt  | uniq -u >  /root/basic-recon/$domain/sources/all.txt &&  cd ../../ 
+
+	done
+
+}
+
+domain_enum
+
+resolving_domains(){
+
+	for domain in $(cat $range);
+
+	do
+
+		printf "\n\n-------------------------------------------------------------\n"
+		printf "          \e[33mBurp Forcing The Subdomain Is Starting...\e[0m                  "
+		printf "\n---------------------------------------------------------------\n\n"
+
+		shuffledns -d $domain -list $domain/sources/all.txt -o $domain/domains.txt -r $resolvers
+
+	done
+
+}
+
+resolving_domains
+
+http_prob(){
+
+	for domain in $(cat $range);
+
+	do
+
+		printf "\n\n---------------------------------------------\n"
+		printf "          \e[33mHTTPS Probe Is Starting ...\e[0m                  "
+		printf "\n---------------------------------------------\n\n"
+
+		cat $domain/domains.txt | httprobe | tee $domain/Recon/url.txt
+
+	done
+
+}
+
+http_prob
+DNSMAP_start(){
+
+	for domain in $(cat $range);
+
+	do
+
+
+   printf "\n\n---------------------------------------------\n"
+   printf "          \e[33m DNSmap Is Starting ...\e[0m                  "
+   printf "\n---------------------------------------------\n\n"
+
+   dnsmap $domain -r $domain/sources/dnsmap.txt
+
+   sed -i '/^$/d' $domain/sources/dnsmap.txt
+   
+
+done
+
+}
+DNSMAP_start
+DNSEnum(){
+
+	for domain in $(cat $range);
+
+	do
+
+
+   printf "\n\n---------------------------------------------\n"
+   printf "          \e[33m DNSEnum Is Starting ...\e[0m                  "
+   printf "\n---------------------------------------------\n\n"
+
+   dnsenum --noreverse -o $domain/sources/DNSEnum-All.txt $domain --private
+
+done
+
+}
+DNSEnum
+
+scanner(){
+
+	for domain in $(cat $range);
+
+	do
+
+		printf "\n\n---------------------------------------------\n"
+		printf "          \e[33mNuclei Is Starting ...\e[0m                  "
+		printf "\n---------------------------------------------\n\n"
+
+		cat $domain/Recon/url.txt | nuclei -t /root/tools/nuclei-templates/cves  -c 332 -o  $domain/Recon/nuclei/cves.txt
+
+		cat $domain/Recon/url.txt | nuclei -t /root/tools/nuclei-templates/default-logins/ -c 332 -o  $domain/Recon/nuclei/default-logins.txt
+
+		cat $domain/Recon/url.txt | nuclei -t /root/tools/nuclei-templates/dns/ -c 332 -o  $domain/Recon/nuclei/dns.txt
+		
+	    cat $domain/Recon/url.txt | nuclei -t /root/tools/nuclei-templates/cnvd -c 332 -o  $domain/Recon/nuclei/cnvd.txt 
+
+	    cat $domain/Recon/url.txt | nuclei -t /root/tools/nuclei-templates/file -c 332 -o  $domain/Recon/nuclei/file.txt               
+
+	    cat $domain/Recon/url.txt | nuclei -t /root/tools/nuclei-templates/iot -c 332 -o  $domain/Recon/nuclei/iot.txt               
+
+	    cat $domain/Recon/url.txt | nuclei -t /root/tools/nuclei-templates/miscellaneous -c 332 -o  $domain/Recon/nuclei/miscellaneous.txt               
+
+	    cat $domain/Recon/url.txt | nuclei -t /root/tools/nuclei-templates/misconfiguration  -c 332 -o  $domain/Recon/nuclei/misconfiguration.txt               
+	    
+		cat $domain/Recon/url.txt | nuclei -t /root/tools/nuclei-templates/exposed-panels/ -c 332 -o  $domain/Recon/nuclei/exposed-panels.txt
+
+		cat $domain/Recon/url.txt | nuclei -t /root/tools/nuclei-templates/exposures/ -c 332 -o  $domain/Recon/nuclei/exposures.txt
+ 
+		cat $domain/Recon/url.txt | nuclei -t /root/tools/nuclei-templates/network/ -c 332 -o  $domain/Recon/nuclei/network.txt
+
+		cat $domain/Recon/url.txt | nuclei -t /root/tools/nuclei-templates/takeovers/ -c 332 -o  $domain/Recon/nuclei/takeovers.txt
+
+		cat $domain/Recon/url.txt | nuclei -t /root/tools/nuclei-templates/technologies/ -c 332 -o  $domain/Recon/nuclei/technologies.txt
+
+		cat $domain/Recon/url.txt | nuclei -t /root/tools/nuclei-templates/vulnerabilities/ -c 332 -o  $domain/Recon/nuclei/vulnerabilities.txt
+
+		find ~/basic-recon/$domain/Recon/nuclei -empty -type f -delete
+
+	done
+}
+
+scanner
+wayback_data(){
+
+	for domain in $(cat $range);
+
+	do
+
+		printf "\n\n---------------------------------------------\n"
+		printf "          \e[33mWaybackurl Is Starting ...\e[0m                  "
+		printf "\n---------------------------------------------\n\n"
+
+		cat $domain/domains.txt | waybackurls | tee $domain/Recon/wayback/tmp.txt
+
+		cat $domain/Recon/wayback/tmp.txt | egrep -v "\.woff|\.svg|\.ttf|\.eot|\.png|\.jpeg|\.jpg|\.css|\.ico" | sed 's/:80//g;s/:443//g' | sort -u >> $domain/Recon/wayback/wayback.txt
+
+		rm $domain/Recon/wayback/tmp.txt
+
+	done
+
+}
+
+wayback_data
+
+valid_urls(){
+
+	for domain in $(cat $range);
+
+	do
+
+		printf "\n\n---------------------------------------------\n"
+		printf "          \e[33mFuzzing Is Starting ...\e[0m                  "
+		printf "\n---------------------------------------------\n\n"
+
+		fuzzer -c -u "FUZZ" -w $domain/Recon/wayback/wayback.txt -of csv -o $domain/Recon/wayback/valid-tmp.txt
+
+		cat $domain/Recon/wayback/valid-tmp.txt | grep http | awk -F "," '{print $1}' >> $domain/Recon/wayback/valid.txt
+
+		rm $domain/Recon/wayback/valid-tmp.txt
+
+	done
+
+}
+
+valid_urls
+
+gf_patterns(){
+
+	for domain in $(cat $range);
+
+	do
+
+		printf "\n\n---------------------------------------------\n"
+		printf "          \e[33mGF Is Starting ...\e[0m                  "
+		printf "\n---------------------------------------------\n\n"
+
+
+		gf xss $domain/Recon/wayback/valid.txt | tee  $domain/Recon/gf/xss.txt
+
+		gf debug_logic $domain/Recon/wayback/valid.txt | tee  $domain/Recon/gf/debug_logic.txt
+
+		gf idor $domain/Recon/wayback/valid.txt | tee  $domain/Recon/gf/idor.txt
+
+		gf img-traversal $domain/Recon/wayback/valid.txt | tee  $domain/Recon/gf/img-traversal.txt
+
+		gf interestingEXT $domain/Recon/wayback/valid.txt | tee  $domain/Recon/gf/interestingEXT.txt
+
+		gf interestingparams $domain/Recon/wayback/valid.txt | tee  $domain/Recon/gf/interestingparams.txt
+
+		gf interestingsubs $domain/Recon/wayback/valid.txt | tee  $domain/Recon/gf/interestingsubs.txt
+
+		gf jsvar $domain/Recon/wayback/valid.txt | tee  $domain/Recon/gf/jsvar.txt
+
+		gf lfi $domain/Recon/wayback/valid.txt | tee  $domain/Recon/gf/lfi.txt
+
+		gf rce $domain/Recon/wayback/valid.txt | tee  $domain/Recon/gf/rce.txt
+
+		gf redirect $domain/Recon/wayback/valid.txt | tee  $domain/Recon/gf/redirect.txt
+
+		gf sqli $domain/Recon/wayback/valid.txt | tee  $domain/Recon/gf/sqli.txt
+
+		gf ssrf $domain/Recon/wayback/valid.txt | tee  $domain/Recon/gf/ssrf.txt
+
+		gf aws-keys $domain/Recon/wayback/valid.txt | tee  $domain/Recon/gf/aws-keys.txt
+
+		gf s3-buckets $domain/Recon/wayback/valid.txt | tee  $domain/Recon/gf/s3-buckets.txt
+
+		gf servers $domain/Recon/wayback/valid.txt | tee  $domain/Recon/gf/servers.txt
+
+		gf debug-pages $domain/Recon/wayback/valid.txt | tee  $domain/Recon/gf/debug-pages.txt
+
+		gf upload-fields $domain/Recon/wayback/valid.txt | tee  $domain/Recon/gf/upload-fields.txt
+
+		gf php-sources $domain/Recon/wayback/valid.txt | tee  $domain/Recon/gf/php-sources.txt
+
+		gf takeovers $domain/Recon/wayback/valid.txt | tee  $domain/Recon/gf/takeovers.txt
+
+		gf firebase $domain/Recon/wayback/valid.txt | tee  $domain/Recon/gf/firebase.txt
+
+		find ~/basic-recon/$domain/Recon/gf -empty -type f -delete
+
+	done
+
+}
+
+gf_patterns
+
+custom_worlis(){
+
+	for domain in $(cat $range);
+
+	do
+		printf "\n\n---------------------------------------------\n"
+		printf "          \e[33mMAKING coustem wordlist  ...\e[0m                  "
+		printf "\n---------------------------------------------\n\n"
+
+		cat $domain/Recon/wayback/wayback.txt | unfurl -unique paths > $domain/Recon/wordlist/path.txt 
+
+		cat $domain/Recon/wayback/wayback.txt | unfurl -unique keys > $domain/Recon/wordlist/params.txt
+
+	done
+
+
+}
+
+custom_worlis
+
+get_ip(){
+
+	for domain in $(cat $range);
+
+	do
+		printf "\n\n---------------------------------------------\n"
+		printf "          \e[33m Starting get_ip ...\e[0m                  "
+		printf "\n---------------------------------------------\n\n"
+
+		$resolve_domain $domain/Recon/masscan/results.txt  $domain/domains.txt
+
+		gf ip $domain/Recon/masscan/results.txt | sort -u > $domain/Recon/masscan/ip.txt
+		
+
+	done
+}
+get_ip
+sub_domina-take(){
+
+	for domain in $(cat $range);
+
+	do
+
+		printf "\n\n---------------------------------------------\n"
+		printf "          \e[33mSubdomain takeovers Starting ...\e[0m                  "
+		printf "\n---------------------------------------------\n\n"
+
+		subzy -targets $domain/domains.txt | tee $domain/Recon/vulnerabilities/takeovers.txt
+
+	done
+}
+sub_domina-take
+
+hidden(){
+
+	for domain in $(cat $range);
+
+	do
+		printf "\n\n---------------------------------------------\n"
+		printf "          \e[33mDirsearch  Is Starting ...\e[0m                  "
+		printf "\n---------------------------------------------\n\n"    
+
+		dirsearch -u $domain -t 100 -o /root/basic-recon/$domain/Recon/hidden_dir/$domain.txt
+
+	done
+}
+hidden
+API_secret(){
+
+	for domain in $(cat $range);
+
+	do
+
+	  printf "\n\n---------------------------------------------\n"
+		printf "          \e[33mAPI_secrets ...\e[0m                  "
+		printf "\n---------------------------------------------\n\n"
+
+		cd  /root/basic-recon/$domain/Recon/API_secret
+
+		cp /root/basic-recon/$domain/Recon/wayback/wayback.txt /root/basic-recon/$domain/Recon/API_secret/wayback.txt
+
+		gau $domain -v -t 100 | tee  gau.txt
+
+		sort gau.txt wayback.txt  | uniq -u > uniq_files.txt
+
+		cat uniq_files.txt | egrep -v "\.woff|\.svg|\.ttf|\.eot|\.png|\.jpeg|\.jpg|\.css|\.ico" >> wayback_only_html.txt
+
+		cat uniq_files.txt  | grep "\.js" | uniq | sort >> wayback_js_files.txt
+ 
+        cat uniq_files.txt  | grep ".json" | uniq | sort >> wayback_json_files.txt
+
+		grep -i -E "admin|auth|api|jenkins|corp|dev|stag|stg|prod|sandbox|swagger|aws|azure|uat|test|vpn|cms" wayback_only_html.txt >> important_http_urls.txt
+
+		grep -i -E  "aws|s3" uniq_files.txt >> aws_s3_files.txt
+
+		cat wayback_only_html.txt 
+
+        find ~/basic-recon/$domain/Recon/API_secret/ -empty -type f -delete
+ 
+		cd /root/basic-recon/
+
+	done
+}
+API_secret
+
+api_domains(){
+
+    for domain in $(cat $range);
+
+	do
+		printf "\n\n---------------------------------------------\n"
+		printf "          \e[33mapi_domain finding ...\e[0m                  "
+		printf "\n---------------------------------------------\n\n"
+
+        cat $domain/domains.txt | grep api | tee /root/basic-recon/$domain/Recon/API_secret/api_domain.txt
+
+	done
+}
+api_domains
+
+
+find_xss(){
+
+
+	for domain in $(cat $range);
+
+	do
+
+		printf "\n\n---------------------------------------------\n"
+		printf "          \e[33mXSS find_xss Is Starting ...\e[0m                  "
+		printf "\n---------------------------------------------\n\n"
+
+        cat $domain/Recon/gf/xss.txt | grep "source=" |  qsreplace '"><script>confirm(1)</script>' | while read host do ; do curl --silent --path-as-is --insecure "$host" | grep -qs "<script>confirm(1)" && echo "$host \033[0;31mVulnerable\n" || echo "$host \033[0;32mNot Vulnerable\n" | >>  /root/basic-recon/$domain/Recon/vulnerabilities/sourse_xss.txt ; done               
+		
+		cat $domain/Recon/url.txt |  dalfox pipe | tee $domain/Recon/Recon/vulnerabilities/dalfox_xss.txt  
+
+	done
+
+}
+find_xss
+Heardbleeds_vulnerability(){
+
+	for domain in $(cat $range);
+
+	do
+
+
+        cat $domain/Recon/url.txt | while read line ; do echo "QUIT" | openssl s_client -connect $line:443 2>&1 | grep 'server extension "heartbeat" (id=15)' || echo $line: safe  >> /root/basic-recon/$domain/Recon/vulnerabilities/heardbled.txt | grep safe; done
+
+     done 
+
+}
+
+Heardbleeds_vulnerability
+github_secrets(){
+
+	for domain in $(cat $range);
+
+	do
+
+		printf "\n\n---------------------------------------------\n"
+		printf "          \e[33mGitdorking  Is Starting ...\e[0m                  "
+		printf "\n---------------------------------------------\n\n"
+
+		cd /root/tools/git-hound
+
+		./git-hound --subdomain-file /root/basic-recon/$domain/domains.txt  --pages 10  >>  /root/basic-recon/$domain/Recon/git-dorking/dorking.txt                  
+	
+		cd /root/basic-recon/
+
+	done
+}
+github_secrets
+bucket_finder(){
+
+    for domain in $(cat $range);
+
+	do
+		printf "\n\n---------------------------------------------\n"
+		printf "          \e[33mbucketfinding  Is Starting ...\e[0m                  "
+		printf "\n---------------------------------------------\n\n"
+
+		cd /root/tools/lazys3
+
+		ruby lazys3.rb $domain >> /root/basic-recon/$domain/sources/bucket.txt
+
+		cd /root/basic-recon/
+
+	done
+
+
+}
+bucket_finder
+clickjacking(){
+
+    for domain in $(cat $range);
+
+	do
+		printf "\n\n---------------------------------------------\n"
+		printf "          \e[33 clickjacking Is Starting ...\e[0m                  "
+		printf "\n---------------------------------------------\n\n"
+
+		cd /root/tools/Clickjacking-Tester
+
+		python3 Clickjacking_Tester.py /root/basic-recon/$domain/Recon/url.txt | tee /root/basic-recon/$domain/Recon/vulnerabilities/clickjacking.txt
+
+		cd /root/basic-recon/$domain/Recon/vulnerabilities/
+
+        cat clickjacking.txt | cut -c 5- > clickjacking.txt
+
+		cd /root/basic-recon/
+
+	done
+
+
+}
+clickjacking
+
+open_redirect(){
+
+    for domain in $(cat $range);
+
+	do
+		printf "\n\n---------------------------------------------\n"
+		printf "          \e[33mopen redirect finding ...\e[0m                  "
+		printf "\n---------------------------------------------\n\n"
+
+       cat /root/basic-recon/$domain/Recon/wayback/wayback.txt | grep -a -i \=http | qsreplace 'http://evil.com' | while read host do;do curl -s -L $host -I | grep "evil.com" && echo "$host\033[0;31mvulnerable\n" | tee /root/basic-recon/$domain/Recon/vulnerabilities/open-redirect.txt ; done 
+
+	done
+}
+open_redirect
+
+
+
+not testing
+
+eyewitness_api(){
+
+	for domain in $(cat $range);
+
+	do
+
+		printf "\n\n-------------------------------------------------------------\n"
+		printf "          \e[33mapi domain scrren short..\e[0m                  "
+		printf "\n---------------------------------------------------------------\n\n"
+
+
+
+		eyewitness -f $domain/domains.txt/api.txt --web
+
+
+	done
+
+}
+
+eyewitness_api
+
+
+
+eyewitness(){
+
+	for domain in $(cat $range);
+
+	do
+
+		printf "\n\n-------------------------------------------------------------\n"
+		printf "          \e[33mapi domain scrren short..\e[0m                  "
+		printf "\n---------------------------------------------------------------\n\n"
+
+		eyewitness -f /root/basic-recon/$domain/domains.txt --web
+
+
+	done
+
+}
+
+eyewitness
